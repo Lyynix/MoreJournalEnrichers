53826d50e9983fdb22bbcf68309f8e0c866fe658
flux <fabian.lux@exxcellent.de>
2023-06-21 19:21:40 +0200

    changed Post to Em to emphasize given text

--- scripts/init.js
+++ scripts/init.js
@@ -2,17 +2,10 @@
 
 CONFIG.TextEditor.enrichers.push(
     {
-        pattern: /@Post\[(.*?)\]/g,
+        pattern: /@Em\[(.*?)\]/g,
         enricher: async(match, options) => {
             const content = match[1]
             return $(`
-            <div class="row-section wrap maskfield postChatSection">
-                <div class="col ninety"></div>
-                <div class="col ten center postContentChat" data-tooltip="SHEET.PostItem">
-                    <em class="far fa-comment-dots"></em>
-                </div>
-                <div class="col postChatContent">${content}</div>
-            </div>
-            `)[0]
+            <em>${content}</em>`)[0]
         }
     })
