431e2c7261623e6fab897f7f9975c229e915fbad
flux <fabian.lux@exxcellent.de>
2023-06-21 20:19:30 +0200

    added SceneMenu enricher for testing

--- scripts/init.js
+++ scripts/init.js
@@ -6,6 +6,20 @@ CONFIG.TextEditor.enrichers.push(
         enricher: async(match, options) => {
             const content = match[1]
             return $(`
-            <em>${content}</em>`)[0]
+            <a class="content-link"><em>${content}</em>`)[0]
+        }
+    },
+    {
+        pattern: /@SceneMenu\[(.*?)\]/g,
+        enricher: async(match, options) => {
+            const uuid = match[1]
+            return $(`<a  class="content-link" 
+                draggable="true" 
+                data-uuid="Scene.${uuid}" 
+                data-id="${uuid}" 
+                data-type="Scene" 
+                data-tooltip="Scene">
+                    <i class="fas fa-map"></i>main
+            </a>`)[0]
         }
     })
