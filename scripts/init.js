8cc92614b3cb7ccf80f0b42534a24ffe19691c7b
flux <fabian.lux@exxcellent.de>
2023-06-21 02:22:12 +0200

    Added dsa5 enricher @PostChat for research

--- scripts/init.js
+++ scripts/init.js
@@ -1,1 +1,18 @@
 // Module specific code goes here. See https://foundryvtt.com/article/module-development/ for help.
+
+CONFIG.TextEditor.enrichers.push(
+    {
+        pattern: /@Post\[(.*?)\]/g,
+        enricher: async(match, options) => {
+            const content = match[1]
+            return $(`
+            <div class="row-section wrap maskfield postChatSection">
+                <div class="col ninety"></div>
+                <div class="col ten center postContentChat" data-tooltip="SHEET.PostItem">
+                    <em class="far fa-comment-dots"></em>
+                </div>
+                <div class="col postChatContent">${content}</div>
+            </div>
+            `)[0]
+        }
+    })
