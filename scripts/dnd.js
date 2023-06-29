a3e8f822b4afb5bbc24289c7d48f1ff609adc3eb
Lyynix <luxfabianl@gmail.com>
2023-06-29 11:01:44 +0200

    added structure for dnd enrichers

---
+++ scripts/dnd.js
@@ -0,0 +1,502 @@
+Hooks.on("init", () => {
+  if (game.system.id === "dnd5e") {
+    console.log("LMJE | Initializing enrichers for dnd5e");
+
+    CONFIG.TextEditor.enrichers.push({
+      pattern: /@Character\[([a-zA-Z0-9]+)\]/g,
+      enricher: async (match, options) => {
+        var html = "testing";
+        return $(html)[0];
+      },
+    });
+  }
+});
+/*
+game.characters.get(id)
+/* JSON 
+dndCharacter = {
+  name: "New Actor",
+  type: "character",
+  _id: "gKH34kbx0TvVQtQf",
+  img: "icons/svg/mystery-man.svg",
+  system: {
+    abilities: {
+      str: {
+        value: 10,
+        proficient: 0,
+        bonuses: {
+          check: "",
+          save: "",
+        },
+      },
+      dex: {
+        value: 10,
+        proficient: 0,
+        bonuses: {
+          check: "",
+          save: "",
+        },
+      },
+      con: {
+        value: 10,
+        proficient: 0,
+        bonuses: {
+          check: "",
+          save: "",
+        },
+      },
+      int: {
+        value: 10,
+        proficient: 0,
+        bonuses: {
+          check: "",
+          save: "",
+        },
+      },
+      wis: {
+        value: 10,
+        proficient: 0,
+        bonuses: {
+          check: "",
+          save: "",
+        },
+      },
+      cha: {
+        value: 10,
+        proficient: 0,
+        bonuses: {
+          check: "",
+          save: "",
+        },
+      },
+    },
+    attributes: {
+      ac: {
+        flat: null,
+        calc: "default",
+        formula: "",
+      },
+      hp: {
+        value: 20,
+        max: 20,
+        temp: null,
+        tempmax: null,
+        bonuses: {
+          level: "",
+          overall: "",
+        },
+      },
+      init: {
+        ability: "dex",
+        bonus: "",
+      },
+      movement: {
+        burrow: 0,
+        climb: 0,
+        fly: 0,
+        swim: 0,
+        walk: 30,
+        units: "ft",
+        hover: false,
+      },
+      attunement: {
+        max: 3,
+      },
+      senses: {
+        darkvision: 0,
+        blindsight: 0,
+        tremorsense: 0,
+        truesight: 0,
+        units: "ft",
+        special: "",
+      },
+      spellcasting: "int",
+      death: {
+        success: 0,
+        failure: 0,
+      },
+      exhaustion: 0,
+      inspiration: false,
+    },
+    details: {
+      biography: {
+        value: "",
+        public: "",
+      },
+      alignment: "",
+      race: "",
+      background: "",
+      originalClass: "",
+      xp: {
+        value: 0,
+      },
+      appearance: "",
+      trait: "",
+      ideal: "",
+      bond: "",
+      flaw: "",
+    },
+    traits: {
+      size: "med",
+      di: {
+        value: ["psychic"],
+        bypasses: ["mgc"],
+        custom: "",
+      },
+      dr: {
+        value: [],
+        bypasses: [],
+        custom: "",
+      },
+      dv: {
+        value: [],
+        bypasses: [],
+        custom: "",
+      },
+      ci: {
+        value: [],
+        custom: "",
+      },
+      languages: {
+        value: ["common"],
+        custom: "",
+      },
+      weaponProf: {
+        value: [],
+        custom: "",
+      },
+      armorProf: {
+        value: [],
+        custom: "",
+      },
+    },
+    currency: {
+      pp: 0,
+      gp: 0,
+      ep: 0,
+      sp: 0,
+      cp: 0,
+    },
+    skills: {
+      acr: {
+        value: 0,
+        ability: "dex",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+      ani: {
+        value: 0,
+        ability: "wis",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+      arc: {
+        value: 0,
+        ability: "int",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+      ath: {
+        value: 0,
+        ability: "str",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+      dec: {
+        value: 0,
+        ability: "cha",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+      his: {
+        value: 0,
+        ability: "int",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+      ins: {
+        value: 0,
+        ability: "wis",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+      itm: {
+        value: 0,
+        ability: "cha",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+      inv: {
+        value: 0,
+        ability: "int",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+      med: {
+        value: 0,
+        ability: "wis",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+      nat: {
+        value: 0,
+        ability: "int",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+      prc: {
+        value: 0,
+        ability: "wis",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+      prf: {
+        value: 0,
+        ability: "cha",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+      per: {
+        value: 0,
+        ability: "cha",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+      rel: {
+        value: 0,
+        ability: "int",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+      slt: {
+        value: 0,
+        ability: "dex",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+      ste: {
+        value: 0,
+        ability: "dex",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+      sur: {
+        value: 0,
+        ability: "wis",
+        bonuses: {
+          check: "",
+          passive: "",
+        },
+      },
+    },
+    tools: {},
+    spells: {
+      spell1: {
+        value: 0,
+        override: null,
+      },
+      spell2: {
+        value: 0,
+        override: null,
+      },
+      spell3: {
+        value: 0,
+        override: null,
+      },
+      spell4: {
+        value: 0,
+        override: null,
+      },
+      spell5: {
+        value: 0,
+        override: null,
+      },
+      spell6: {
+        value: 0,
+        override: null,
+      },
+      spell7: {
+        value: 0,
+        override: null,
+      },
+      spell8: {
+        value: 0,
+        override: null,
+      },
+      spell9: {
+        value: 0,
+        override: null,
+      },
+      pact: {
+        value: 0,
+        override: null,
+      },
+    },
+    bonuses: {
+      mwak: {
+        attack: "",
+        damage: "",
+      },
+      rwak: {
+        attack: "",
+        damage: "",
+      },
+      msak: {
+        attack: "",
+        damage: "",
+      },
+      rsak: {
+        attack: "",
+        damage: "",
+      },
+      abilities: {
+        check: "",
+        save: "",
+        skill: "",
+      },
+      spell: {
+        dc: "",
+      },
+    },
+    resources: {
+      primary: {
+        value: null,
+        max: null,
+        sr: false,
+        lr: false,
+        label: "",
+      },
+      secondary: {
+        value: null,
+        max: null,
+        sr: false,
+        lr: false,
+        label: "",
+      },
+      tertiary: {
+        value: null,
+        max: null,
+        sr: false,
+        lr: false,
+        label: "",
+      },
+    },
+  },
+  prototypeToken: {
+    name: "New Actor",
+    displayName: 0,
+    actorLink: true,
+    appendNumber: false,
+    prependAdjective: false,
+    texture: {
+      src: "icons/svg/mystery-man.svg",
+      scaleX: 1,
+      scaleY: 1,
+      offsetX: 0,
+      offsetY: 0,
+      rotation: 0,
+    },
+    width: 1,
+    height: 1,
+    lockRotation: false,
+    rotation: 0,
+    alpha: 1,
+    disposition: 1,
+    displayBars: 0,
+    bar1: {
+      attribute: "attributes.hp",
+    },
+    bar2: {
+      attribute: null,
+    },
+    light: {
+      alpha: 0.5,
+      angle: 360,
+      bright: 0,
+      coloration: 1,
+      dim: 0,
+      attenuation: 0.5,
+      luminosity: 0.5,
+      saturation: 0,
+      contrast: 0,
+      shadows: 0,
+      animation: {
+        type: null,
+        speed: 5,
+        intensity: 5,
+        reverse: false,
+      },
+      darkness: {
+        min: 0,
+        max: 1,
+      },
+    },
+    sight: {
+      enabled: true,
+      range: 0,
+      angle: 360,
+      visionMode: "basic",
+      attenuation: 0.1,
+      brightness: 0,
+      saturation: 0,
+      contrast: 0,
+    },
+    detectionModes: [],
+    flags: {},
+    randomImg: false,
+  },
+  items: [],
+  effects: [],
+  folder: null,
+  sort: 0,
+  ownership: {
+    default: 0,
+    vHns02qqy1nzi1Gg: 3,
+  },
+  flags: {
+    dnd5e: {},
+  },
+  _stats: {
+    systemId: "dnd5e",
+    systemVersion: "2.2.1",
+    coreVersion: "11.302",
+    createdTime: 1687657183315,
+    modifiedTime: 1687657433774,
+    lastModifiedBy: "vHns02qqy1nzi1Gg",
+  },
+};
+*/
--- scripts/init.js
+++ scripts/init.js
@@ -1,29 +1,33 @@
 // Module specific code goes here. See https://foundryvtt.com/article/module-development/ for help.
 
-CONFIG.TextEditor.enrichers.push(
-  {
-    pattern: /@SceneMenu\[((\s*[a-zA-Z0-9]+)+\s*?)\]/g,
-    enricher: async (match, options) => {
-      const uuids = match[1].split(/\s+/g);
+Hooks.on("init", () => {
+  console.log("LMJE | Initializing generic enrichers");
 
-      var menuHtml = /* html */ `
+  CONFIG.TextEditor.enrichers.push(
+    //#region @SceneMenu[...]
+    {
+      pattern: /@SceneMenu\[((\s*[a-zA-Z0-9]+)+\s*?)\]/g,
+      enricher: async (match, options) => {
+        const uuids = match[1].split(/\s+/g);
+
+        var menuHtml = /* html */ `
       <table>
         <tr>
           <td colspan="2">${game.i18n.localize("LMJE.SCENEMENU.Title")}</td>
         </tr>
       `;
 
-      for (var i = 0; i < uuids.length; i++) {
-        var uuid = uuids[i];
+        for (var i = 0; i < uuids.length; i++) {
+          var uuid = uuids[i];
 
-        var sceneDocument = game.scenes.get(uuid);
-        if (!sceneDocument) continue;
+          var sceneDocument = game.scenes.get(uuid);
+          if (!sceneDocument) continue;
 
-        var sceneName = sceneDocument.navName
-          ? `${sceneDocument.navName} (${sceneDocument.name})`
-          : sceneDocument.name;
+          var sceneName = sceneDocument.navName
+            ? `${sceneDocument.navName} (${sceneDocument.name})`
+            : sceneDocument.name;
 
-        var sceneControl = /* html */ `
+          var sceneControl = /* html */ `
         <a title="${game.i18n.localize(
           "LMJE.SCENEMENU.Tooltip.Show"
         )}" onclick="
@@ -59,62 +63,64 @@ CONFIG.TextEditor.enrichers.push(
         </a>
         `;
 
-        menuHtml += /* html */ `    
+          menuHtml += /* html */ `    
         <tr>
           <td>${sceneName}</td>
           <td style="display: flex; justify-content: flex-end">${sceneControl}</td>
         </tr>
         `;
-      }
+        }
 
-      menuHtml += /* html */ `
+        menuHtml += /* html */ `
       </table>
       `;
 
-      return $(menuHtml)[0];
+        return $(menuHtml)[0];
+      },
     },
-  },
-  {
-    pattern: /(@ToC)(?!\[)/g,
-    enricher: async (match, options) => {
-      //console.log(options.relativeTo.parent);
+    //#endregion
+    //#region @ToC
+    {
+      pattern: /(@ToC)(?!\[)/g,
+      enricher: async (match, options) => {
+        //console.log(options.relativeTo.parent);
 
-      var tocHtml = ``;
+        var tocHtml = ``;
 
-      var journal = options.relativeTo.parent;
-      var thisPageId = options.relativeTo._id;
+        var journal = options.relativeTo.parent;
+        var thisPageId = options.relativeTo._id;
 
-      var pages = journal.pages
-        .map((e) => e)
-        .sort((a, b) => {
-          return a.sort - b.sort;
-        });
-      //console.log(pages);
+        var pages = journal.pages
+          .map((e) => e)
+          .sort((a, b) => {
+            return a.sort - b.sort;
+          });
+        //console.log(pages);
 
-      var prevTitleLevel = 0;
-      pages.forEach((page) => {
-        if (prevTitleLevel < page.title.level) {
-          for (let i = 0; i < page.title.level - prevTitleLevel; i++) {
-            tocHtml += /* html */ `
+        var prevTitleLevel = 0;
+        pages.forEach((page) => {
+          if (prevTitleLevel < page.title.level) {
+            for (let i = 0; i < page.title.level - prevTitleLevel; i++) {
+              tocHtml += /* html */ `
               <ul style="list-style: none;">
             `;
-          }
-        } else if (prevTitleLevel > page.title.level) {
-          for (let i = 0; i < prevTitleLevel - page.title.level; i++) {
-            tocHtml += /* html */ `
+            }
+          } else if (prevTitleLevel > page.title.level) {
+            for (let i = 0; i < prevTitleLevel - page.title.level; i++) {
+              tocHtml += /* html */ `
               </ul>
             `;
+            }
           }
-        }
-        tocHtml += /* html */ `
+          tocHtml += /* html */ `
           <li>
             <a class="content-link"
               style="background: none; border: none; font-size: ${
                 (4 - page.title.level) * 3 + 13
               }pt"
               data-uuid="JournalEntry.${journal._id}.JournalEntryPage.${
-          page._id
-        }"
+            page._id
+          }"
               data-id="${page._id}"
               data-type="JournalEntryPage"
               data-tooltip="${journal.name}: ${page.name}">
@@ -123,55 +129,57 @@ CONFIG.TextEditor.enrichers.push(
           </li>
         `;
 
-        prevTitleLevel = page.title.level;
-      });
+          prevTitleLevel = page.title.level;
+        });
 
-      tocHtml += /* html */ `
+        tocHtml += /* html */ `
         </ul>
       `;
 
-      return $(tocHtml)[0];
+        return $(tocHtml)[0];
+      },
     },
-  },
-  {
-    pattern: /@ToC\[(\s*[a-zA-Z0-9]+)\]/g,
-    enricher: async (match, options) => {
-      var tocHtml = ``;
+    //#endregion
+    //#region @ToC[journalID]
+    {
+      pattern: /@ToC\[(\s*[a-zA-Z0-9]+)\]/g,
+      enricher: async (match, options) => {
+        var tocHtml = ``;
 
-      //console.log(match)
-      var journal = game.journal.get(match[1]);
+        //console.log(match)
+        var journal = game.journal.get(match[1]);
 
-      var pages = journal.pages
-        .map((e) => e)
-        .sort((a, b) => {
-          return a.sort - b.sort;
-        });
-      //console.log(pages);
+        var pages = journal.pages
+          .map((e) => e)
+          .sort((a, b) => {
+            return a.sort - b.sort;
+          });
+        //console.log(pages);
 
-      var prevTitleLevel = 0;
-      pages.forEach((page) => {
-        if (prevTitleLevel < page.title.level) {
-          for (let i = 0; i < page.title.level - prevTitleLevel; i++) {
-            tocHtml += /* html */ `
+        var prevTitleLevel = 0;
+        pages.forEach((page) => {
+          if (prevTitleLevel < page.title.level) {
+            for (let i = 0; i < page.title.level - prevTitleLevel; i++) {
+              tocHtml += /* html */ `
               <ul style="list-style: none;">
             `;
-          }
-        } else if (prevTitleLevel > page.title.level) {
-          for (let i = 0; i < prevTitleLevel - page.title.level; i++) {
-            tocHtml += /* html */ `
+            }
+          } else if (prevTitleLevel > page.title.level) {
+            for (let i = 0; i < prevTitleLevel - page.title.level; i++) {
+              tocHtml += /* html */ `
               </ul>
             `;
+            }
           }
-        }
-        tocHtml += /* html */ `
+          tocHtml += /* html */ `
           <li>
             <a class="content-link"
               style="background: none; border: none; font-size: ${
                 (4 - page.title.level) * 3 + 13
               }pt"
               data-uuid="JournalEntry.${journal._id}.JournalEntryPage.${
-          page._id
-        }"
+            page._id
+          }"
               data-id="${page._id}"
               data-type="JournalEntryPage"
               data-tooltip="${journal.name}: ${page.name}">
@@ -180,14 +188,16 @@ CONFIG.TextEditor.enrichers.push(
           </li>
         `;
 
-        prevTitleLevel = page.title.level;
-      });
+          prevTitleLevel = page.title.level;
+        });
 
-      tocHtml += /* html */ `
+        tocHtml += /* html */ `
         </ul>
       `;
 
-      return $(tocHtml)[0];
-    },
-  }
-);
+        return $(tocHtml)[0];
+      },
+    }
+    //#endregion
+  );
+});
