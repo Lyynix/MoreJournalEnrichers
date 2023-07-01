dc903adb97c971c145dea4c45375d69936181bc9
Lyynix <luxfabianl@gmail.com>
2023-07-02 01:13:57 +0200

    Finished basic DnD Enricher and translations

--- scripts/dnd.js
+++ scripts/dnd.js
@@ -1,21 +1,41 @@
 Hooks.on("init", () => {
+  loadTemplates(["modules/lyynix-more-journal-enrichers/templates/characterDnD.html"])
+
   if (game.system.id === "dnd5e") {
     console.log("LMJE | Initializing enrichers for dnd5e");
 
     CONFIG.TextEditor.enrichers.push({
-      pattern: /@Character\[([a-zA-Z0-9]+)\](\{(((attr|bio)\s?)+)\})?/g,
+      pattern: /@Character\[([a-zA-Z0-9]+)\]/g/*(\{(((attr|bio)\s?)+)\})?*/,
       enricher: async (match, options) => {
-        var char = game.characters.get(match[1])
-        var charOptions = match[3].split(/\s/g)
 
-        var html = "<p>testing</p>";
-        return $(html)[0];
+        var char = game.actors.get(match[1])
+        //var charOptions = match[3].split(/\s/g)
+        console.log(char)
+
+        var templateEntries = {
+          id: char._id,
+          name: char.name,
+          img: char.img,
+          race: char.system.details.race,
+          alignment: char.system.details.alignment,
+          stats: {
+            str: char.system.abilities.str.value,
+            dex: char.system.abilities.dex.value,
+            con: char.system.abilities.con.value,
+            int: char.system.abilities.int.value,
+            wis: char.system.abilities.wis.value,
+            cha: char.system.abilities.cha.value,
+          }
+        }
+        var returnHtml = await renderTemplate("modules/lyynix-more-journal-enrichers/templates/characterDnD.html", templateEntries)
+        return $(returnHtml)[0];
       },
     });
   }
 });
+
 /*
-game.characters.get(id)
+game.actors.get(id)
 /* JSON 
 dndCharacter = {
   name: "New Actor",
