aaab886daed82b99e65f1f016ec7dd480f027835
Lyynix <luxfabianl@gmail.com>
2023-07-11 23:17:20 +0200

    added error handling

--- scripts/init.js
+++ scripts/init.js
@@ -6,64 +6,86 @@ import { sceneMenu, inlineScene } from "./enrichers/sceneEnrichers.js";
 import { patterns, templates } from "./helpers.js";
 
 Hooks.on("init", () => {
-  console.log("LMJE | Loading templates");
-  loadTemplates([templates.whisperTable, templates.chatTable]);
+  try {
+    loadTemplates([templates.whisperTable, templates.chatTable]);
+    console.log("LMJE | Loaded templates");
+  } catch (error) {
+    console.error("LMJE | Failed to load templates\n", error);
+  }
 
-  console.log("LMJE | Initializing generic enrichers");
-  CONFIG.TextEditor.enrichers.push(
-    {
-      pattern: patterns.scene.menu,
-      enricher: sceneMenu,
-    },
-    {
-      pattern: patterns.toc,
-      enricher: toc,
-    },
-    {
-      pattern: patterns.scene.inline,
-      enricher: inlineScene,
-    },
-    {
-      pattern: patterns.playlist.inline,
-      enricher: inlinePlaylist,
-    },
-    {
-      pattern: patterns.playlist.menu,
-      enricher: playlistMenu,
-    },
-    {
-      pattern: patterns.chat.whisper,
-      enricher: whisper,
-    },
-    {
-      pattern: patterns.chat.chat,
-      enricher: chat,
-    }
-  );
+  try {
+    CONFIG.TextEditor.enrichers.push(
+      {
+        pattern: patterns.scene.menu,
+        enricher: sceneMenu,
+      },
+      {
+        pattern: patterns.toc,
+        enricher: toc,
+      },
+      {
+        pattern: patterns.scene.inline,
+        enricher: inlineScene,
+      },
+      {
+        pattern: patterns.playlist.inline,
+        enricher: inlinePlaylist,
+      },
+      {
+        pattern: patterns.playlist.menu,
+        enricher: playlistMenu,
+      },
+      {
+        pattern: patterns.chat.whisper,
+        enricher: whisper,
+      },
+      {
+        pattern: patterns.chat.chat,
+        enricher: chat,
+      }
+    );
+    console.log("LMJE | Initialized generic enrichers");
+  } catch (error) {
+    console.error("LMJE | Failed to initialize generic enrichers\n", error);
+  }
 
   switch (game.system.id) {
     case "dnd5e":
-      console.log("LMJE | Loading templates for dnd5e");
-      loadTemplates([templates.character.dnd]);
+      try {
+        loadTemplates([templates.character.dnd]);
+        console.log("LMJE | Loaded templates for dnd5e");
+      } catch (error) {
+        console.error("LMJE | Failed to load dnd5 templates\n", error);
+      }
 
-      console.log("LMJE | Initializing enrichers for dnd5e");
-
-      CONFIG.TextEditor.enrichers.push({
-        pattern: patterns.character,
-        enricher: characterDnD,
-      });
+      try {
+        CONFIG.TextEditor.enrichers.push({
+          pattern: patterns.character,
+          enricher: characterDnD,
+        });
+        console.log("LMJE | Initialized enrichers for dnd5e");
+      } catch (error) {
+        console.error("LMJE | Failed to initialize dnd5 enrichers\n", error);
+      }
       break;
 
     case "pf2e":
-      console.log("LMJE | Loading templates for pf2e");
-      loadTemplates([templates.character.pf2e]);
+      try {
+        loadTemplates([templates.character.pf2e]);
+        console.log("LMJE | Loaded templates for pf2e");
+      } catch (error) {
+        console.error("LMJE | Failed to load pf2e templates\n", error);
+      }
 
-      console.log("LMJE | Initializing enrichers for pf2e");
-
-      CONFIG.TextEditor.enrichers.push({
-        pattern: patterns.character,
-        enricher: characterPF2e,
-      });
+      try {
+        CONFIG.TextEditor.enrichers.push({
+          pattern: patterns.character,
+          enricher: characterPF2e,
+        });
+        console.log("LMJE | Initialized enrichers for pf2e");
+      } catch (error) {
+        console.error("LMJE | Failed to initialize pf2e enrichers\n", error);
+      }
 
     default:
       break;
