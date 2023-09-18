6b35555265c7b319a2d8a1f7ef8719091dd3ebed
Lyynix <luxfabianl@gmail.com>
2023-09-18 03:02:01 +0200

    generate data object for rolltable full

--- scripts/enrichers/rolltableEnrichers.js
+++ scripts/enrichers/rolltableEnrichers.js
@@ -2,39 +2,105 @@ import { EnricherPattern } from "../enricherPattern.js";
 import { getDocument, templates } from "../helpers.js";
 
 export async function rolltableFull(match, options) {
-  return "RT FULL"
+  var id = match[1];
+  var rolltable;
+  try {
+    rolltable = await getDocument(id, "RollTable");
+  } catch (error) {
+    return $(invalidHtml(game.i18n.localize(error)))[0];
+  }
+
+  var rolltableName = match[2] === undefined ? rolltable.name : match[2];
+
+  var rolltableData = {
+    label: rolltableName,
+    documentData: {
+      uuid: rolltable.uuid,
+      id: rolltable.id,
+      type: rolltable.documentName,
+    },
+    formula: rolltable.formula,
+    description: rolltable.description,
+    results: [],
+  };
+
+  var results = await rolltable.results.contents;
+  for (let i = 0; i < results.length; i++) {
+    const result = results[i];
+    var resultData = {
+      type: result.type,
+      range: result.range,
+      text: result.text,
+      img: result.img,
+    };
+
+    switch (result.type) {
+      case 1:
+        var doc = game.collections
+          .get(result.documentCollection)
+          .get(result.documentId);
+        resultData.document = {
+          id: result.documentId,
+          uuid: doc.uuid,
+          type: result.documentCollection,
+        };
+        break;
+
+      case 2:
+        var doc = game.packs
+          .get(result.documentCollection)
+          .index.get(result.documentId);
+        resultData.document = {
+          id: result.documentId,
+          uuid: doc.uuid,
+          type: result.documentCollection,
+        };
+        break;
+
+      default:
+        break;
+    }
+
+    rolltableData.results.push(resultData);
+  }
+
+  // console.log("LMJE | ", rolltable);
+  // console.log("LMJE | ", rolltableData);
+
+  return $(await renderTemplate(templates.rolltable.full, rolltableData))[0];
 }
 
 export async function rolltableMenu(match, options) {
   const ids = match[1].split(EnricherPattern.SEPARATOR);
-  const title = match[2] !== undefined ? match[2] : game.i18n.localize("LMJE.PLAYLIST.Title");
+  const title =
+    match[2] !== undefined
+      ? match[2]
+      : game.i18n.localize("LMJE.PLAYLIST.Title");
 
   var menuData = {
     title: title,
-    rolltables: []
-  } 
+    rolltables: [],
+  };
   for (let i = 0; i < ids.length; i++) {
     const id = ids[i];
     var document;
-    var label
-    var docId
+    var label;
+    var docId;
     try {
-      document = (await getDocument(id, "RollTable"));
-      label = document.name
-      docId = document.id
+      document = await getDocument(id, "RollTable");
+      label = document.name;
+      docId = document.id;
     } catch (error) {
-      label = game.i18n.localize(error)
-      docId = ""
+      label = game.i18n.localize(error);
+      docId = "";
     }
-    menuData.rolltables.push(
-      {
-        label: label,
-        id: docId
-      }
-    )
+    menuData.rolltables.push({
+      label: label,
+      id: docId,
+    });
   }
 
-  return $(await renderTemplate(templates.rolltable.menu, menuData))[0]
+  return $(await renderTemplate(templates.rolltable.menu, menuData))[0];
 }
 
 export async function rolltableInline(match, options) {
@@ -54,7 +120,7 @@ export async function rolltableInline(match, options) {
     documentData: {
       uuid: rolltable.uuid,
       id: rolltable.id,
-      type: rolltable.documentName
+      type: rolltable.documentName,
     },
     buttons: [
       {
@@ -63,7 +129,7 @@ export async function rolltableInline(match, options) {
         onclick: `
           game.tables.get('${rolltable.id}').draw();
           return false;
-        `
+        `,
       },
       {
         tooltip: "Edit",
@@ -71,10 +137,10 @@ export async function rolltableInline(match, options) {
         onclick: `
           new RollTableConfig(game.tables.get('${rolltable.id}')).render(true);
           return false;
-        `
-      }
-    ]
-  }
+        `,
+      },
+    ],
+  };
 
-  return $(await renderTemplate(templates.inline, rolltableData))[0]
+  return $(await renderTemplate(templates.inline, rolltableData))[0];
 }
