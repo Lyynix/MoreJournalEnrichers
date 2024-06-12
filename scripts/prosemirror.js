fb1896037d54af8ef80b3e91a6e609000f5b9f52
Lyynix <luxfabianl@gmail.com>
2024-06-12 21:47:58 +0200

    finished functional document selector

--- scripts/prosemirror.js
+++ scripts/prosemirror.js
@@ -1,57 +1,8 @@
 import { templates } from "./helpers.js";
 
-var functions = {
-  prosemirror: null,
-  insertToC: async function () {
-    console.log("LMJE |", this.prosemirror);
-
-    selectDocument();
-
-    this.prosemirror.view.dispatch(
-      this.prosemirror.view.state.tr
-        .insertText(`@ToC[${await navigator.clipboard.readText()}]`)
-        .scrollIntoView()
-    );
-  },
-};
-
-function selectDocument() {
-  // array to store all maximized windows
-  let prevWindows = [];
-
-  // loop over all windows
-  Object.values(ui.windows).forEach(w => {
-    // check if window is maximized
-    if (w._minimized) return;
-    // add maximized window and position to list
-    prevWindows.push({
-      window: w,
-      position: { ...w.position }
-    });
-
-    // minimize window and move to top left
-    w.minimize();
-    w.setPosition({ ...w.position, ...{ left: 0, top: 0 } });
-  });
-
-  
-  setTimeout(() => {
-    prevWindows.forEach(prev => {
-      prev.window.setPosition(prev.position);
-      prev.window.maximize();
-    });
-  }, 2000);
-}
-
 export function initProsemirrorButtons() {
-  // Hooks.on("createProseMirrorEditor", (...args) => {
-  //   console.log("LMJE |", args)
-  // })
-  // Hooks.on("getProseMirrorMenuItems", (...args) => {
-  //   console.log("LMJE |", args)
-  // })
   Hooks.on("getProseMirrorMenuDropDowns", (proseMirrorMenu, dropdowns) => {
-    console.log("LMJE |", proseMirrorMenu, dropdowns);
+    // console.log("LMJE |", proseMirrorMenu, dropdowns);
 
     let options = {
       prosemirror: proseMirrorMenu,
@@ -63,10 +14,8 @@ export function initProsemirrorButtons() {
       title: "Enricher",
       entries: [
         {
-          action: "table of content",
-          active: false,
-          group: 1,
           title: "Table of Content",
+          action: "toc",
           cmd: functions.insertToC.bind(options),
         },
       ],
@@ -74,25 +23,86 @@ export function initProsemirrorButtons() {
   });
 }
 
-class InsertEnrichFVerForm extends FormApplication {
-  constructor(options) {
-    super();
-    this.options = options;
-  }
+async function selectDocument(expectedType) {
+  // array to store all maximized windows
+  let prevMaximized = []
+  let prevWindows = []
 
-  static get defaultOptions() {
-    return mergeObject(super.defaultOptions, {
-      classes: ["form"],
-      height: 200,
-      width: 400,
-      popOut: true,
-      template: templates.prosemirror.insertEnricher,
-      id: "insert-enricher",
-      title: "LMJE.PROSEMIRROR.InsertEnricher",
+  // loop over all windows
+  Object.values(ui.windows).forEach(async (w) => {
+    prevWindows.push(w)
+    if (w._minimized) return
+
+    // add maximized window and position to list
+    prevMaximized.push({
+      window: w,
+      position: { ...w.position },
+      minimized: w._minimized,
+      id: w.id,
     });
-  }
 
-  async _updateObject() {
-    this.render();
-  }
+    // minimize window and move to top left
+    await w.maximize()
+    w.setPosition({ ...w.position, ...{ left: 0, top: 0 } });
+    w.minimize();
+  });
+
+  let promiseResolve;
+  let promiseReject;
+  let result = new Promise((resolve, reject) => {
+    promiseResolve = resolve;
+    promiseReject = reject;
+  });
+
+  // Hooks on open Document Sheet (needs to be edited)
+  Hooks.once("renderDocumentSheet", (config, dom, options) => {
+    setTimeout(
+      () => {
+        Object.values(ui.windows).forEach(window => {
+          let included = false
+          prevWindows.forEach(prevWindow => {
+            if (window.id === prevWindow.id) {
+              included = true;
+              // console.log("windows have same id: ", window, prevWindow)
+            }
+
+          })
+          if (!included) window.close() 
+        });
+
+        prevMaximized.forEach((prev) => {
+          prev.window.setPosition(prev.position);
+          if (!prev.minimized) prev.window.maximize();
+        });
+    
+        if (config.document.documentName === expectedType) {
+          promiseResolve(config.document);
+        } else {
+          promiseReject("Wrong Type");
+        }
+      },
+      500
+    )
+  });
+
+  return result;
 }
+
+var functions = {
+  prosemirror: null,
+  insertToC: function () {
+    // console.log("LMJE |", this.prosemirror);
+    selectDocument("JournalEntry")
+      .then((document) => {
+        // console.log(document);
+        this.prosemirror.view.dispatch(
+          this.prosemirror.view.state.tr
+            .insertText(`@ToC[${document.uuid}]`)
+            .scrollIntoView()
+        );
+      })
+      .catch((reason) => {
+        ui.notifications.warn("Test Warning: " + reason);
+      });
+  },
+};
